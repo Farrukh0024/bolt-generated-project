33a47d4376133cded0a9fb6f32175a6f8ec55d35
Farrukh0024 <154988444+Farrukh0024@users.noreply.github.com>
2025-02-21 17:08:39 +0500

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000
+  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    minify: true,
-    chunkSizeWarningLimit: 1600,
-  },
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
